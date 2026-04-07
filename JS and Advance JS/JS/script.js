47a8457707e4ac389c823fe89d4b9cfac3d094ba
Vaishnavi Manohar Lakade <112229914+VaishnaviLakade@users.noreply.github.com>
2026-04-07 17:39:33 +0530

    Add files via upload

---
+++ JS and Advance JS/JS/script.js
@@ -0,0 +1,47 @@
+// 1. What is JavaScript
+console.log(
+  "JavaScript is a scripting language used to make web pages interactive.",
+);
+
+// 2. Features of JavaScript
+console.log("Features: Lightweight, Interpreted, Dynamic, Event-driven");
+
+// 3. Variables
+let name = "Vaishnavi";
+const age = 21;
+var city = "Pune";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+// 4. Operators
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+// 5. Loops
+
+// for loop
+for (let i = 1; i <= 3; i++) {
+  console.log("For Loop:", i);
+}
+
+// while loop
+let i = 1;
+while (i <= 3) {
+  console.log("While Loop:", i);
+  i++;
+}
+
+// 6. Function (Integration with HTML)
+function showMessage() {
+  alert("JavaScript is integrated with HTML using script tag!");
+}
+
+// 7. How JS is integrated
+console.log("JavaScript is added using <script> tag inside HTML.");
