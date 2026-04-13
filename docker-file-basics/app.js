d5ad46fabea7d10b4767d3c6b28ddd287f2ca3bd
Vaishnavi Manohar Lakade <112229914+VaishnaviLakade@users.noreply.github.com>
2026-04-13 16:04:43 +0530

    Delete docker-file-basics directory

--- docker-file-basics/app.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("Docker app is running");
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
